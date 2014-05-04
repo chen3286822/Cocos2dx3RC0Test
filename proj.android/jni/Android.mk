7fd68f5a2e1b752bc39f3fd8e90fbc39d89ab0a9
chenfeifei <chen3286822@yeah.net>
2014-05-04 21:27:43 +0800

    编译安卓版本，添加必要的库和资源

--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -12,15 +12,22 @@ LOCAL_SRC_FILES := hellocpp/main.cpp \
 				   ../../Classes/Card.cpp \
 				   ../../Classes/Dialog.cpp
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes \
+				../../cocos2d/extensions \
+				../../cocos2d/cocos/editor-support/cocostudio \
+				../../cocos2d/cocos \
+				../../cocos2d
 
 LOCAL_WHOLE_STATIC_LIBRARIES := cocos2dx_static
 LOCAL_WHOLE_STATIC_LIBRARIES += cocosdenshion_static
 LOCAL_WHOLE_STATIC_LIBRARIES += box2d_static
-
+LOCAL_WHOLE_STATIC_LIBRARIES += cocostudio_static
+LOCAL_WHOLE_STATIC_LIBRARIES += cocos_extension_static
 
 include $(BUILD_SHARED_LIBRARY)
 
 $(call import-module,2d)
 $(call import-module,audio/android)
 $(call import-module,Box2D)
+$(call import-module,editor-support/cocostudio)
+$(call import-module,extensions)
